819e4980ff3c154dcee0381d3e9c5682004bef9a
1603884387 <1603884387@qq.com>
2024-05-06 16:52:09 +0800

    root

---
+++ js/index01.js
@@ -0,0 +1,15 @@
+"use strict";
+var md5 = function (key, value) {
+    return key + value;
+};
+const arr = [{
+        name: '张三',
+        age: 30,
+    }, {
+        name: '李四',
+        age: 40,
+    }, {
+        name: '王五',
+        age: 50,
+    }];
+console.log(arr);
